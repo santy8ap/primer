f270068194716f5914c829094bb2b01032c7bf14
Coders <santy8aposso@gmail.com>
2025-04-30 07:04:54 -0500

    actualizacion

--- modulo1_sem2/entrenamiento.py
+++ modulo1_sem2/entrenamiento.py
@@ -1,18 +1,31 @@
-nota1 = int(input("ingrese su nota de la materia del 0 al 100: "))
+# nota1 = int(input("ingrese su nota de la materia del 0 al 100: "))
 
 
 
-if nota1 < 70:
-    print("usted a reprobado")
+# if nota1 < 70:
+#     print("usted a reprobado")
 
-elif nota1 == 100:
-    print("usted saco la nota maxima")
+# elif nota1 == 100:
+#     print("usted saco la nota maxima")
 
-elif nota1 >= 70:
-    print("usted aprobo la materia")
+# elif nota1 >= 70:
+#     print("usted aprobo la materia")
 
-else:
-    print("nota no válida")
+# else:
+#     print("nota no válida")
 
 
-print("cambio de prueba")
+print("cambio de prueba34")
+
+
+x = 0
+
+while x < 100:
+    print("X tiene un valor de ", x )
+    # x += 1 (hace lo mismo que la liena de abajo)
+    x = x+1
+    if x == 50:
+        break
+
+
+
