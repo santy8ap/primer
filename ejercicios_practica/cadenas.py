39a4671798569dc215687a5d21586815cb95eb58
Coders <santy8aposso@gmail.com>
2025-05-07 13:50:02 -0500

    ejercicio_semanal

--- ejercicios_practica/cadenas.py
+++ ejercicios_practica/cadenas.py
@@ -1,88 +1,155 @@
-#Escribir un programa que pregunte el nombre de
-#usuario en la consola y un número entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.
+# #Escribir un programa que pregunte el nombre de
+# #usuario en la consola y un número entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.
 
-name = input("Como te llamas: ")
-n = input("introduce un numero entero: ")
-print ((name + "\n") * int(n))
-
-
-#Escribir un programa que pregunte el nombre completo del usuario en la consola y después muestre por pantalla el nombre completo del usuario tres veces,
-#una con todas las letras minúsculas, otra con todas las letras mayúsculas y otra solo con la primera letra de
-#nombre y de los apellidos en mayúscula. El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
-
-name = input("como te llamas? ")
-print(name.lower())
-print(name.upper())
-print(name.title())
-
-#Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre 
-# por pantalla <NOMBRE> tiene <n> letras, donde <NOMBRE> es el nombre de usuario en mayúsculas y <n> es el número de letras que tienen el nombre.
-
-name = input("¿Como te llamas? ")
-print(name.upper() + " tiene " + str(len(name)) + " letras ")
-
-#Los teléfonos de una empresa tienen el siguiente formato prefijo-número-extension donde el prefijo es el código del país +34,
-#  y la extensión tiene dos dígitos (por ejemplo +34-913724710-56). Escribir un programa
-#  que pregunte por un número de teléfono con este formato y muestre por pantalla el número de teléfono sin el prefijo y la extensión.
-
-tel = input("Introduce un numero de telefono con el formato +57 ")
-print('El numero de telefono es: ', tel[4:-3])
+# name = input("Como te llamas: ")
+# n = input("introduce un numero entero: ")
+# print ((name + "\n") * int(n))
 
 
+# #Escribir un programa que pregunte el nombre completo del usuario en la consola y después muestre por pantalla el nombre completo del usuario tres veces,
+# #una con todas las letras minúsculas, otra con todas las letras mayúsculas y otra solo con la primera letra de
+# #nombre y de los apellidos en mayúscula. El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
 
-#Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla la frase invertida.
+# name = input("como te llamas? ")
+# print(name.lower())
+# print(name.upper())
+# print(name.title())
 
-frase = input("Introduce una frase: ")
-print(frase[::-1])
+# #Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre 
+# # por pantalla <NOMBRE> tiene <n> letras, donde <NOMBRE> es el nombre de usuario en mayúsculas y <n> es el número de letras que tienen el nombre.
 
-#Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal en minúscula,
-#  y después muestre por pantalla la misma frase pero con la vocal introducida en mayúscula.+
+# name = input("¿Como te llamas? ")
+# print(name.upper() + " tiene " + str(len(name)) + " letras ")
 
-frase = input("Introduce una frase: ")
-vocal = input("introduce una vocal en minuscula: ")
-print(frase.replace(vocal, vocal.upper()))
+# #Los teléfonos de una empresa tienen el siguiente formato prefijo-número-extension donde el prefijo es el código del país +34,
+# #  y la extensión tiene dos dígitos (por ejemplo +34-913724710-56). Escribir un programa
+# #  que pregunte por un número de teléfono con este formato y muestre por pantalla el número de teléfono sin el prefijo y la extensión.
 
-
-#Escribir un programa que pregunte el correo electrónico del usuario en la consola y muestre por 
-# pantalla otro correo electrónico con el mismo nombre (la parte delante de la arroba @) pero con dominio riwi.es.
-
-email = input("Introduce el nombre que quieres para tu correo: ")
-print ("tu correo es: " + email[:email.find('@')] + '@Riwi.io')
-
-#Escribir un programa que pregunte por consola el precio de un
-#producto en euros con dos decimales y muestre por pantalla el número de euros y el número de céntimos del precio introducido.
-
-precio = input("introduce el precio en euros con 2 decimanles: ")
-print("el precio es: " + precio[:precio.find('.')], 'euros y', precio[precio.find('.') + 1:], "centimos")
-
-
-#Escribir un programa que pregunte al usuario la fecha de su nacimiento en formato dd/mm/aaaa y muestra por pantalla, el día, el mes y el año.
-
-fecha = input("introduce tu fecha de nacimiento en formato dia/mes/año: ")
-dia = fecha [:fecha.find('/')]
-mes_a = fecha [:fecha.find('/')+1:]
-mes =  mes_a [:mes_a.find('/')]
-year = mes_a [:mes_a.find('/') +1]
-print('dia', dia)
-print('mes', mes)
-print('año', year)
-
-
-#Escribir un programa que pregunte por consola por los productos de una cesta de la compra,
-#  separados por comas, y muestre por pantalla cada uno de los productos en una línea distinta.
-
-cesta = input('Introduce los productos de la cesta de la compra separados por comas: ')
-print(cesta.replace(',', '\n'))
+# tel = input("Introduce un numero de telefono con el formato +57 ")
+# print('El numero de telefono es: ', tel[4:-3])
 
 
 
-#Escribir un programa que pregunte el nombre el un producto, su precio y un número de unidades y muestre por pantalla una cadena con el siguiente formato:
+# #Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla la frase invertida.
 
-#<producto>: <unidades> unidades x <precio>€ = <total>€
+# frase = input("Introduce una frase: ")
+# print(frase[::-1])
 
-#donde <unidades> es el número de unidades con cinco dígitos, <precio> es el precio unitario con 6 dígitos enteros y 2 decimales y <total> es el coste total con 8 dígitos enteros y 2 decimales.
+# #Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal en minúscula,
+# #  y después muestre por pantalla la misma frase pero con la vocal introducida en mayúscula.+
 
-producto = input('Introduce el nombre del producto: ')
-precio = float(input('Introduce el precio unitario: '))
-unidades = int(input('Introduce el numero de unidades: '))
-print('{producto}: {unidades:3d} unidades x {precio:9.2f}€ = {total:11.2f}€'.format(producto = producto, unidades = unidades, precio = precio, total = unidades * precio))
+# frase = input("Introduce una frase: ")
+# vocal = input("introduce una vocal en minuscula: ")
+# print(frase.replace(vocal, vocal.upper()))
+
+
+# #Escribir un programa que pregunte el correo electrónico del usuario en la consola y muestre por 
+# # pantalla otro correo electrónico con el mismo nombre (la parte delante de la arroba @) pero con dominio riwi.es.
+
+# email = input("Introduce el nombre que quieres para tu correo: ")
+# print ("tu correo es: " + email[:email.find('@')] + '@Riwi.io')
+
+# #Escribir un programa que pregunte por consola el precio de un
+# #producto en euros con dos decimales y muestre por pantalla el número de euros y el número de céntimos del precio introducido.
+
+# precio = input("introduce el precio en euros con 2 decimanles: ")
+# print("el precio es: " + precio[:precio.find('.')], 'euros y', precio[precio.find('.') + 1:], "centimos")
+
+
+# #Escribir un programa que pregunte al usuario la fecha de su nacimiento en formato dd/mm/aaaa y muestra por pantalla, el día, el mes y el año.
+
+# fecha = input("introduce tu fecha de nacimiento en formato dia/mes/año: ")
+# dia = fecha [:fecha.find('/')]
+# mes_a = fecha [:fecha.find('/')+1:]
+# mes =  mes_a [:mes_a.find('/')]
+# year = mes_a [:mes_a.find('/') +1]
+# print('dia', dia)
+# print('mes', mes)
+# print('año', year)
+
+
+# #Escribir un programa que pregunte por consola por los productos de una cesta de la compra,
+# #  separados por comas, y muestre por pantalla cada uno de los productos en una línea distinta.
+
+# cesta = input('Introduce los productos de la cesta de la compra separados por comas: ')
+# print(cesta.replace(',', '\n'))
+
+
+
+# #Escribir un programa que pregunte el nombre el un producto, su precio y un número de unidades y muestre por pantalla una cadena con el siguiente formato:
+
+# #<producto>: <unidades> unidades x <precio>€ = <total>€
+
+# #donde <unidades> es el número de unidades con cinco dígitos, <precio> es el precio unitario con 6 dígitos enteros y 2 decimales y <total> es el coste total con 8 dígitos enteros y 2 decimales.
+
+# producto = input('Introduce el nombre del producto: ')
+# precio = float(input('Introduce el precio unitario: '))
+# unidades = int(input('Introduce el numero de unidades: '))
+# print('{producto}: {unidades:3d} unidades x {precio:9.2f}€ = {total:11.2f}€'.format(producto = producto, unidades = unidades, precio = precio, total = unidades * precio))
+
+
+# nombres = {"ana":1,"pepito":2, "juan":3,}
+# print("el tipo de de nombre es: ", type(nombres))
+# for nombre, valor in nombres.items():
+#     print("buenos dias", nombre, valor)
+
+# for i in range(1,10):
+#     if i % 3 == 0 and i % 5 ==0:
+#        print("fizzbazz")
+#     elif i % 3==0:
+#         print("fizz")
+#     elif i % 5==0:
+#         print("buzz")
+#     else:
+#         print(i)
+
+# for x in  range(0,5):
+#     print("hola el numero es: ",x)
+
+# for x in  "hola":
+#     print(x)
+
+#     nombres = ("ana", "juan", "maria")
+
+#     for nombre in nombres:
+#         print(nombre)
+
+# for i in range(5):
+#     print(f"numero de rango {i}")
+# from statistics import mean
+
+# lista = []
+# n = int(input("ingrese la cantidad de notas que desea \
+# promediar:"))
+# for i in range(1, n+1):
+#     lista.append(int(input(f"ingrese la nota numero {i}: ")))
+
+#     lista.sort()
+
+#     print(f"la lista de notas ingresadas {lista}")
+#     print(f"el promedio de las notas es: {mean(lista): .2f}")
+
+def main():
+    print("Pares e impares")
+    
+    numero_1 = int(input("Escriba un numero entero: "))
+    numero_2 = int(input(f"Escriba un numero entero o mayor o igual que {numero_1}: "))
+
+
+
+    while True:
+        if numero_2 < numero_1:
+            print(f"El numero que ingresaste no es mayor que {numero_1}")
+            numero_2 = int(input("Inténtelo otra vez, ingrese un número entero mayor o igual que el primero: "))
+
+        else:
+            for i in range(numero_1, numero_2 + 1):
+                if i % 2 == 0:
+                    print(f"EL NUMERO {i} ES PAR")
+                    numero_1 = int(input("Escriba otro numero entero: "))
+                else:
+                    print(f"EL NUMERO {i} ES IMPAR")
+                    numero_1 = int(input("Escriba otro numero entero: "))
+        
+if __name__ == "__main__":
+    main()
--- modulo1_sem3/ejercicio_semanal.py
+++ modulo1_sem3/ejercicio_semanal.py
@@ -3,110 +3,100 @@ inventario = []
 
 def nombre_producto():
     while True:
-        nombre = input("Ingrese el nombre del producto: ")
-        if nombre.strip() and all(c.isalpha() for c in nombre):
-            return nombre.strip()
-        else:
-            print("El nombre del producto debe contener solo letras y no estar vacío.")
+        nombre = input("Ingrese el nombre del producto: ").strip()
+        if nombre and nombre.isalpha():
+            return nombre
+        print("El nombre del producto debe contener solo letras y no estar vacío.")
 
 def solicitar_precio():
     while True:
-        entrada = input("Ingrese el precio del producto: ")
         try:
-            numero = float(entrada)
-            return numero
+            precio = float(input("Ingrese el precio del producto: "))
+            if precio >= 0:
+                return precio
+            print("El precio no puede ser negativo.")
         except ValueError:
-            print("Precio no válido, ingrese otra vez el precio.")
+            print("Precio no válido, ingrese un número.")
 
 def cantidad_productos():
     while True:
-        cantidad = input("Ingresar cantidad: ")
         try:
-            productos = int(cantidad)
-            if productos >= 0:
-                return productos
-            else:
-                print("La cantidad no puede ser negativa.")
+            cantidad = int(input("Ingrese la cantidad del producto: "))
+            if cantidad >= 0:
+                return cantidad
+            print("La cantidad no puede ser negativa.")
         except ValueError:
-            print("Cantidad no válida, ingrese otra vez.")
+            print("Cantidad no válida, ingrese un número entero.")
 
 def añadir_producto(nombre, precio, cantidad):
     inventario.append({"nombre": nombre, "precio": precio, "cantidad": cantidad})
     print(f"Producto '{nombre}' añadido al inventario.")
 
 def buscar_producto(nombre):
-    while True:
-        try:
-            for producto in inventario:
-                if producto["nombre"].lower() == nombre.lower():
-                    return producto["precio"], producto["cantidad"]
-            print("Producto no encontrado. Intente nuevamente.")
-            nombre = input("Ingrese el nombre del producto a buscar: ").strip()
-        except Exception as e:
-            print(f"Error al buscar el producto: {e}")
+    for producto in inventario:
+        if producto["nombre"].lower() == nombre.lower():
+            return producto
+    return None
 
 def actualizar_precio(nombre, nuevo_precio):
-    while True:
-        for producto in inventario:
-            if producto["nombre"].lower() == nombre.lower():
-                producto["precio"] = nuevo_precio
-                print(f"Precio del producto '{nombre}' actualizado a {nuevo_precio}.")
-                return True
-        print(f"Producto '{nombre}' no encontrado. Intente nuevamente.")
-        nombre = input("Ingrese el nombre del producto a actualizar: ").strip()
-        nuevo_precio = solicitar_precio()
+    producto = buscar_producto(nombre)
+    if producto:
+        producto["precio"] = nuevo_precio
+        print(f"Precio del producto '{nombre}' actualizado a {nuevo_precio}.")
+    else:
+        print(f"Producto '{nombre}' no encontrado.")
 
 def eliminar_producto(nombre):
-    while True:
-        global inventario
-        if any(producto["nombre"].lower() == nombre.lower() for producto in inventario):
-            inventario = [producto for producto in inventario if producto["nombre"].lower() != nombre.lower()]
-            print(f"Producto '{nombre}' eliminado del inventario.")
-            return
-        print(f"Producto '{nombre}' no encontrado. Intente nuevamente.")
-        nombre = input("Ingrese el nombre del producto a eliminar: ").strip()
+    global inventario
+    producto = buscar_producto(nombre)
+    if producto:
+        inventario = [p for p in inventario if p["nombre"].lower() != nombre.lower()]
+        print(f"Producto '{nombre}' eliminado del inventario.")
+    else:
+        print(f"Producto '{nombre}' no encontrado.")
 
 def calcular_valor_total():
+    total = sum(producto["precio"] * producto["cantidad"] for producto in inventario)
+    return total
+
+# Ejemplo de uso
+if __name__ == "__main__":
     while True:
-        try:
-            total = sum(producto["precio"] * producto["cantidad"] for producto in inventario)
-            return total
-        except Exception as e:
-            print(f"Error al calcular el valor total: {e}")
+        print("\nOpciones:")
+        print("1. Añadir producto")
+        print("2. Buscar producto")
+        print("3. Actualizar precio")
+        print("4. Eliminar producto")
+        print("5. Calcular valor total del inventario")
+        print("6. Salir")
+        
+        opcion = input("Seleccione una opción: ").strip()
 
-# Ejecución sin menú:
-
-# Añadir producto
-print("Agregar un nuevo producto:")
-nombre = nombre_producto()
-precio = solicitar_precio()
-cantidad = cantidad_productos()
-añadir_producto(nombre, precio, cantidad)
-
-# Mostrar producto añadido
-print(f"\nProducto añadido: {nombre} - Precio: {precio} - Cantidad: {cantidad}")
-
-# Buscar producto
-print("\nBuscar producto:")
-nombre_buscar = input("Ingrese el nombre del producto a buscar: ").strip()
-resultado = buscar_producto(nombre_buscar)
-if resultado:
-    precio_encontrado, cantidad_encontrada = resultado
-    print(f"Producto encontrado: Precio = {precio_encontrado}, Cantidad = {cantidad_encontrada}")
-else:
-    print("Producto no encontrado.")
-
-# Actualizar precio
-print("\nActualizar precio de un producto:")
-nombre_actualizar = input("Ingrese el nombre del producto a actualizar: ").strip()
-nuevo_precio = solicitar_precio()
-actualizado = actualizar_precio(nombre_actualizar, nuevo_precio)
-
-# Eliminar producto
-print("\nEliminar producto:")
-nombre_eliminar = input("Ingrese el nombre del producto a eliminar: ").strip()
-eliminar_producto(nombre_eliminar)
-
-# Calcular valor total
-total = calcular_valor_total()
-print(f"\nEl valor total del inventario es: {total}")
+        
+        if opcion == "1":
+            nombre = nombre_producto()
+            precio = solicitar_precio()
+            cantidad = cantidad_productos()
+            añadir_producto(nombre, precio, cantidad)
+        elif opcion == "2":
+            nombre = input("Ingrese el nombre del producto a buscar: ").strip()
+            producto = buscar_producto(nombre)
+            if producto:
+                print(f"Producto encontrado: Precio = {producto['precio']}, Cantidad = {producto['cantidad']}")
+            else:
+                print("Producto no encontrado.")
+        elif opcion == "3":
+            nombre = input("Ingrese el nombre del producto a actualizar: ").strip()
+            nuevo_precio = solicitar_precio()
+            actualizar_precio(nombre, nuevo_precio)
+        elif opcion == "4":
+            nombre = input("Ingrese el nombre del producto a eliminar: ").strip()
+            eliminar_producto(nombre)
+        elif opcion == "5":
+            total = calcular_valor_total()
+            print(f"El valor total del inventario es: {total}")
+        elif opcion == "6":
+            print("Saliendo del programa.")
+            break
+        else:
+            print("Opción no válida. Intente nuevamente.")
